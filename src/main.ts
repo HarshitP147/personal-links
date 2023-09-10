99a69cd4e05eb0926483d1893cab055e26e66efe
Harshit Pandit <harshit7757@gmail.com>
2023-09-11 00:46:08 +0100

    Adding custom mouse

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-import anime from "animejs";
-
 const mouse = document.getElementById("mouse")!;
 const nameBox = document.getElementById("name-box")!;
 const modal = document.getElementById("modal-root")!;
@@ -12,37 +10,45 @@ redTheme.style.opacity = '0';
 
 let isBluetheme = true;
 
-window.addEventListener("click",() => {
+window.addEventListener("click", () => {
     isBluetheme = !isBluetheme;
-    if(isBluetheme){
+    if (isBluetheme) {
         blueTheme.style.opacity = '1';
         redTheme.style.opacity = '0'
     }
-    else{
+    else {
         blueTheme.style.opacity = '0'
         redTheme.style.opacity = '1'
     }
 });
 
-nameBox.addEventListener("click",()=>{
+nameBox.addEventListener("click", () => {
     modal.style.animation = "background-in 0.5s";
     modal.classList.remove("hidden");
 });
 
-closeBtn.addEventListener("click",() =>{
+closeBtn.addEventListener("click", () => {
     modal.style.animation = "background-out 0.5s";
-    setTimeout(()=>{
+    setTimeout(() => {
         modal.classList.add("hidden");
-    },490)
+    }, 490)
 })
 
+document.addEventListener('mousemove', e => {
+    mouse.style.transform = `transform3d(calc(${e.clientX}px-50%),calc(${e.clientY}px-50%))`
+})
 
-// window.addEventListener("mousemove",e => {
-//     anime({
-//         targets:mouse,
-//         translateX:e.clientX || e.pageX,
-//         translateY:e.clientY || e.pageY ,
-//         elasticity:10,
-//         duration:650
-//     })
-// })
+document.addEventListener('mousemove', function (e) {
+    mouse.style.left = e.clientX + 'px';
+    mouse.style.top = e.clientY + 'px';
+});
+
+
+document.querySelectorAll('.links').forEach(item => {
+    item.addEventListener('mouseover', () => {
+        mouse.classList.add('hover');
+    });
+    item.addEventListener('mouseleave', () => {
+        mouse.classList.remove('hover');
+    });
+})
